79154fe348673c3cc97d3e66f6092f43748480bf
Eusha Hasan <113366884+eusha06@users.noreply.github.com>
2023-12-07 12:00:57 +0600

    UVA - 10079 - Pizza Cutting .cpp

---
+++ UVA - 10079 - Pizza Cutting .cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define pb push_back
+#define forn(i, n) for(int i = 0; i < int(n); i++)
+#define MOD 1000000007
+#define w(t) int t; cin>>t; while (t--)
+int main() {
+ll n;
+while (cin>>n && n>=0){
+	ll s;
+	s = (n*(n+1))/2;
+	cout<<s+1<<endl;
+}
+return 0;
+}
