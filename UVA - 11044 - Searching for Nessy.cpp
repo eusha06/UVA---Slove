0651176044713f5b2db49d14728036c03dfa806f
Eusha Hasan <113366884+eusha06@users.noreply.github.com>
2024-01-14 01:53:32 +0600

    Create UVA - 11044 - Searching for Nessy.cpp

---
+++ UVA - 11044 - Searching for Nessy.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sakalakabumbum() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long int
+#define pb push_back
+#define ep emplace_back
+#define forn(i, n) for(int i = 0; i < int(n); i++)
+#define MOD 1000000007
+#define w(t) int t; cin >> t; cin.ignore(); while (t--) 
+int main() {
+	w(t){
+int i,n,m;
+cin>>n>>m;
+cout<<(n/3)*(m/3)<<endl;
+}
+    return 0;
+}
