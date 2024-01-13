34985072b62daacfeb50786aea7ce22aa7701122
Eusha Hasan <113366884+eusha06@users.noreply.github.com>
2024-01-13 21:51:33 +0600

    Create UVA - 1124 - Celebrity jeopardy.cpp

---
+++ UVA - 1124 - Celebrity jeopardy.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sakalakabumbum() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long int
+#define pb push_back
+#define ep emplace_back
+#define forn(i, n) for(int i = 0; i < int(n); i++)
+#define MOD 1000000007
+#define w(t) int t; cin >> t; cin.ignore(); while (t--) 
+
+int main() {
+string a;
+int i,n=0;
+while (getline(cin,a)){
+cout<<a<<endl;
+}
+    return 0;
+}
